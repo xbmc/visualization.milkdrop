2b58439472cd59356a6aa56567c36efac1f05479
Anton Fedchin <afedchin@ruswizards.com>
2015-05-26 15:12:53 +0300

    [addon] dx11 related changes.

--- src/MilkdropXBMC.cpp
+++ src/MilkdropXBMC.cpp
@@ -92,7 +92,7 @@ extern "C" ADDON_STATUS ADDON_Create(void* hdl, void* props)
   strcpy(presetsPath, visprops->presets);
 
   Preinit();
-  if(!g_plugin || !g_plugin->PluginInitialize((LPDIRECT3DDEVICE9)visprops->device, visprops->x, visprops->y, visprops->width, visprops->height, visprops->pixelRatio))
+  if(!g_plugin || !g_plugin->PluginInitialize((ID3D11DeviceContext*)visprops->device, visprops->x, visprops->y, visprops->width, visprops->height, visprops->pixelRatio))
     return ADDON_STATUS_UNKNOWN;
 
   return ADDON_STATUS_NEED_SAVEDSETTINGS; // We need some settings to be saved later before we quit this plugin
