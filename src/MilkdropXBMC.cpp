da76e58ee84e427f46acc93585d618673980d7cb
Alwin Esch <alwin.esch@web.de>
2017-04-26 11:40:13 +0200

    Remove not used part

--- src/MilkdropXBMC.cpp
+++ src/MilkdropXBMC.cpp
@@ -277,15 +277,6 @@ extern "C" void ADDON_Destroy()
   ADDON_Stop();
 }
 
-//-- HasSettings --------------------------------------------------------------
-// Returns true if this add-on use settings
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-extern "C" bool ADDON_HasSettings()
-{
-  return true;
-}
-
 //-- GetStatus ---------------------------------------------------------------
 // Returns the current Status of this visualisation
 // !!! Add-on master function !!!
@@ -293,23 +284,6 @@ extern "C" bool ADDON_HasSettings()
 extern "C" ADDON_STATUS ADDON_GetStatus()
 {
   return ADDON_STATUS_OK;
-}
-
-//-- GetSettings --------------------------------------------------------------
-// Return the settings for XBMC to display
-//-----------------------------------------------------------------------------
-
-extern "C" unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet)
-{
-  return 0;
-}
-
-//-- FreeSettings --------------------------------------------------------------
-// Free the settings struct passed from XBMC
-//-----------------------------------------------------------------------------
-
-extern "C" void ADDON_FreeSettings()
-{
 }
 
 //-- UpdateSetting ------------------------------------------------------------
@@ -393,14 +367,6 @@ extern "C" ADDON_STATUS ADDON_SetSetting(const char* id, const void* value)
     return ADDON_STATUS_UNKNOWN;
 
   return ADDON_STATUS_OK;
-}
-
-//-- Announce -----------------------------------------------------------------
-// Receive announcements from XBMC
-//-----------------------------------------------------------------------------
-
-extern "C" void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data)
-{
 }
 
 //-- GetSubModules ------------------------------------------------------------
